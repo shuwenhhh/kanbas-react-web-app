b1b99e00da8232cdf0027b7a0cc04cf5f1326b3a
Shuwen Wang <shuwenwang97@gmail.com>
2023-11-20 23:44:29 -0500

    a5 REST fa23

--- src/envVariables.js
+++ src/envVariables.js
@@ -1,1 +1,1 @@
-export const BACKEND_BASE_URL = process.env.REACT_APP_BACKEND_BASE_URL || "http://localhost:4000/api"
+export const BACKEND_BASE_URL = process.env.REACT_APP_BACKEND_BASE_URL || "https://kanbas-server-node-app-e160875cfba2.herokuapp.com"
