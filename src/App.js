f342048a1d3f5efa210707491c2b02727db57c5d
Shuwen Wang <shuwenwang97@gmail.com>
2023-12-10 09:00:26 -0500

    change package json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import Labs from "./Lab";
- import Kanbas from "./Kanbas";
- import { HashRouter, Route,Routes, Navigate } from 'react-router-dom';
-import Project from './project';
-
 
 function App() {
   return (
-    <HashRouter>
-    <div> 
-        <Routes>   
-        <Route path="/"         element={<Navigate to="/Labs"/>}/>  
-        <Route path="/project/*" element={<Project/>} /> 
-        <Route path="/Labs/*"   element={<Labs/>}/>
-        <Route path="/Kanbas/*" element={<Kanbas/>}/>
-        </Routes> 
-      </div>       
-    </HashRouter>
-
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
