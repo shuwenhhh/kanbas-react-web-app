a612b24a346704309bca4618c1b8da86a0d58fb9
Shuwen Wang <shuwenwang97@gmail.com>
2023-12-10 09:51:59 -0500

    fix node modules

--- src/envVariables.js
+++ src/envVariables.js
@@ -1,1 +1,2 @@
 export const BACKEND_BASE_URL = process.env.REACT_APP_BACKEND_BASE_URL || "https://kanbas-server-node-app-e160875cfba2.herokuapp.com"
+//export const REACT_APP_BASE_API_URL="http://localhost:4000"
